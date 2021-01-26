9963e60b7a284d79001e37a7c6c00115d3be96f7
Elbie-em <emoonga1993@gmail.com>
2021-01-26 02:40:27 +0200

    Fix lint errors

--- src/index.js
+++ src/index.js
@@ -1,30 +1,34 @@
-import data from './data/inkjetData'
-import * as doman from './modules/doman'
+import data from './data/inkjetData';
+import * as doman from './modules/doman';
 
 const setTitle = () => {
   const title = doman.getElement('app-name');
-  doman.setValue(title,data.name)
-}
+  doman.setValue(title, data.name);
+};
 
 const getImpact = () => {
   const selectBox = doman.getElement('impacts');
-  if(selectBox.value === "climate_change"){
-    const {production, distribution, use, end_of_life} = data.impacts.climate_change
-    doman.setData('production',production);
-    doman.setData('distribution',distribution);
-    doman.setData('use',use)
-    doman.setData('end_of_life',end_of_life)
-  }else if(selectBox.value === "energy_use"){
-    const {production, distribution, use, end_of_life} = data.impacts.energy_use
-    doman.setData('production',production);
-    doman.setData('distribution',distribution);
-    doman.setData('use',use)
-    doman.setData('end_of_life',end_of_life)
+  if (selectBox.value === 'climate_change') {
+    const {
+      production, distribution, use, end_of_life, // eslint-disable-line camelcase
+    } = data.impacts.climate_change;
+    doman.setData('production', production);
+    doman.setData('distribution', distribution);
+    doman.setData('use', use);
+    doman.setData('end_of_life', end_of_life);
+  } else if (selectBox.value === 'energy_use') {
+    const {
+      production, distribution, use, end_of_life, // eslint-disable-line camelcase
+    } = data.impacts.energy_use;
+    doman.setData('production', production);
+    doman.setData('distribution', distribution);
+    doman.setData('use', use);
+    doman.setData('end_of_life', end_of_life);
   }
-}
+};
 
-window.onload = (event) => {
+window.onload = () => {
   setTitle();
-  getImpact()
-  doman.setChangeEvent('#impacts',getImpact);
+  getImpact();
+  doman.setChangeEvent('#impacts', getImpact);
 };
--- src/modules/doman.js
+++ src/modules/doman.js
@@ -1,16 +1,16 @@
 export const getElement = (id) => {
   const el = document.getElementById(id);
-  return el
-}
+  return el;
+};
 
-export const setValue = (el,value) => {
+export const setValue = (el, value) => {
   el.innerText = value;
-}
+};
 
-export const setChangeEvent = (id,func) => {
-  document.querySelector(id).addEventListener('change',func)
-}
+export const setChangeEvent = (id, func) => {
+  document.querySelector(id).addEventListener('change', func);
+};
 
-export const setData = (id,data) => {
-  document.getElementById(id).style.height = `${data}%`
-}
+export const setData = (id, data) => {
+  document.getElementById(id).style.height = `${data}%`;
+};
